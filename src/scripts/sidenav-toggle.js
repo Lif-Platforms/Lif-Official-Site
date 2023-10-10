7f9efb6d120bc8a35a72a02ec9f311bc8a928783
Superior125 <90008321+Superior126@users.noreply.github.com>
2023-10-10 09:10:18 -0400

    Updates to sidenav/topnav

--- src/scripts/sidenav-toggle.js
+++ src/scripts/sidenav-toggle.js
@@ -5,3 +5,11 @@ function OpenSideNav() {
 function CloseSideNav() {
     document.getElementById("side-nav").className = "hamburger-menu-hide";
 }
+
+function ToggleSideNav() {
+    if (document.getElementById('side-nav').classList.contains('hamburger-menu-hide')) {
+        document.getElementById('side-nav').className = "hamburger-menu-show";
+    } else {
+        document.getElementById('side-nav').className = "hamburger-menu-hide";
+    }
+}
