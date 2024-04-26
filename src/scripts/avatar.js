94b409d62309fe851606cc14eeb0a79c938bf951
Superior125 <90008321+Superior126@users.noreply.github.com>
2024-04-25 20:57:03 -0400

    Updates

--- src/scripts/avatar.js
+++ src/scripts/avatar.js
@@ -1,8 +1,38 @@
+function toggle_account_panel() {
+    const account_panel = document.getElementById("account_panel");
+
+    // Check class name
+    if (account_panel.className === "closed") {
+        account_panel.className = "open";
+
+    } else {
+        account_panel.className = "closed";
+    }
+
+}
+
 // Authenticate with auth server
 fetch("https://api.auth.lifplatforms.com/auth/verify_token")
 .then((response) => {
-    if (!response.ok) {
+    if (response.ok) {
+        // Get username
+        const username = Cookies.get("LIF_USERNAME");
+
         // Update avatar image
-        document.getElementById("user-avatar").innerHTML = "<img src='https://api.auth.lifplatforms.com/profile/get_avatar/null'>";
+        document.getElementById("user-avatar").innerHTML = `
+                                                            <img onclick='toggle_account_panel()' src='https://api.auth.lifplatforms.com/profile/get_avatar/null' alt='User Avatar'>
+                                                            <iframe class='closed' id='account_panel' src='https://api.auth.lifplatforms.com/profile/get_profile/${username}?service_url="https://lifplatforms.com'></iframe>
+                                                            `;
+    } else {
+        throw new Error("Request failed with status code: " + response.status);
     }
+})
+.catch((error) => {
+    console.error(error);
+
+    // Update avatar image
+    document.getElementById("user-avatar").innerHTML = `
+                                                        <img onclick='toggle_account_panel()' src='https://api.auth.lifplatforms.com/profile/get_avatar/null' alt='User Avatar'>
+                                                        <iframe class='closed' id='account_panel' src='https://api.auth.lifplatforms.com/profile/get_profile/guest?service_url="https://lifplatforms.com'></iframe>
+                                                       `;
 })
