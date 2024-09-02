d5ebcd4baae1b353165b89fbf15f4063c480ec38
Superior125 <90008321+Superior126@users.noreply.github.com>
2024-09-02 17:27:31 -0400

    Prepare for production

--- server.js
+++ server.js
@@ -213,4 +213,7 @@ app.all('*', (req, res) => {
     }
 })
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
