35ae54b85f275de9278c3ca38a6458df5ba514a4
Superior125 <90008321+Superior126@users.noreply.github.com>
2025-05-07 00:57:38 -0400

    FIx code scanning issues

--- public/scripts/cookie-banner-toggle.js
+++ public/scripts/cookie-banner-toggle.js
@@ -1,19 +1,17 @@
 // Wait for page to fully load before checking cookie status
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
     // Get user cookie accept cookie
-    const cookie = Cookies.get('User-Cookie-Accept');
-
-    console.log(cookie);
+    const cookie = Cookies.get("User-Cookie-Accept");
 
     // Check if user accepted the use of cookies
     if (!cookie) {
         // Show cookie banner
-        document.getElementById('cookie-banner').className = 'cookie-banner-open';
+        document.getElementById("cookie-banner").className = "cookie-banner-open";
     }
 });
 
 // Sets user accept cookie and close cookie banner
 function accept_cookies() {
-    document.getElementById('cookie-banner').className = 'cookie-banner-closed';
-    Cookies.set('User-Cookie-Accept');
+    document.getElementById('cookie-banner').className = "cookie-banner-closed";
+    Cookies.set("User-Cookie-Accept");
 }
